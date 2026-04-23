f429a018619a8d7486c7093043204ac5a567969a
ankitrathore6430 <111001263+ankitrathore6430@users.noreply.github.com>
2026-04-23 10:54:23 +0530

    Update firebase-messaging-sw.js

--- Solor/firebase-messaging-sw.js
+++ Solor/firebase-messaging-sw.js
@@ -1,27 +1,25 @@
 importScripts('https://www.gstatic.com/firebasejs/9.22.0/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/9.22.0/firebase-messaging-compat.js');
 
-// Initialize the Firebase app in the service worker
-firebase.initializeApp({
+const firebaseConfig = {
     apiKey: "AIzaSyA3pH_dnb6_--wRoDSuoj-TAudsmJ7D4R0",
     authDomain: "solor-energy.firebaseapp.com",
     projectId: "solor-energy",
     storageBucket: "solor-energy.firebasestorage.app",
     messagingSenderId: "772737546715",
     appId: "1:772737546715:web:3736545b464523bdc04ffe"
-});
+};
 
+firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage((payload) => {
-    console.log('[firebase-messaging-sw.js] Received background message ', payload);
-    
-    const notificationTitle = payload.notification.title;
-    const notificationOptions = {
-        body: payload.notification.body,
-        icon: '/icon.png', // You can change this to your app icon path
-        badge: '/icon.png'
-    };
+  console.log('[firebase-messaging-sw.js] Received background message ', payload);
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: '/icon.png' // अपना आइकॉन पाथ डालें
+  };
 
-    self.registration.showNotification(notificationTitle, notificationOptions);
+  self.registration.showNotification(notificationTitle, notificationOptions);
 });
